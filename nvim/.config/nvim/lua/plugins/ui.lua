0490ff0b1fcd3ee366fde04e4854df836950776a
Lukas Wagner <mail@lukaswagner.dev>
2023-03-15 21:16:03 +0100

    minimal lazy.vim setup, setup catpuccin theme

---
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,11 @@
+-- vim: sw=2 expandtab:
+
+return {
+  { 
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      vim.cmd.colorscheme "catppuccin-macchiato"
+    end,
+  }
+}
